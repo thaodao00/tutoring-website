2f3521bf6be2bbc001a545a868d80ce2000b0413
User <19130087@st.hcmuaf.edu.vn>
2022-11-06 22:26:25 +0700

    ++page info-user

---
+++ src/config/routes.js
@@ -0,0 +1,22 @@
+const routes = {
+    home: '/home',
+    class: '/class',
+    tutor: '/tutor',
+    login: '/login',
+    logout: '/logout',
+    register: '/register',
+    posts: '/posts',
+    contacts: '/contacts',
+    searchTutor: '/searchTutor',
+    registerAsTutor: '/register-as-tutor',
+    ReferenceTuition: '/reference-tuition',
+    contract: '/contract',
+    tutorDetail: '/tutor-detail',
+    infoUser: '/info-user',
+    infoLogin: '/info-login',
+    listClassTeach: '/list-class-teach',
+    listClassStudy: '/list-class-study',
+
+
+}
+export default routes;
