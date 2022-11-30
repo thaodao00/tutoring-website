e197f93de2547334639f7d4d841f5b639cf79e1f
thanhlocpc <nguyenthanhlocpc@gmail.com>
2022-11-30 17:00:26 +0700

    ++edit domain

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://tutor-services.herokuapp.com/tutor-service/';
+const BASE_URL = 'https://tutor-service.azurewebsites.net/tutor-service/';
 // const BASE_URL = process.env.REACT_APP_API_URL
 const instance = axios.create({
     baseURL: BASE_URL,
