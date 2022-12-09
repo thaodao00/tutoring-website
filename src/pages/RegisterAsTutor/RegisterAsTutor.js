c6ac1c02442886443442327e53391b1a559323b6
Đào Thị Thu Thảo <91473426+thaodao00@users.noreply.github.com>
2022-12-09 10:49:44 +0700

    ++ up

--- src/pages/RegisterAsTutor/RegisterAsTutor.js
+++ src/pages/RegisterAsTutor/RegisterAsTutor.js
@@ -12,16 +12,19 @@ import DateItem from "~/pages/RegisterAsTutor/DateItem";
 import OptionItem from "~/pages/ReferenceTuition/OptionItem";
 import DayTutor from "~/layout/common/DayTutor";
 import { becomeTutor, fetchLevel, fetchSubject, getAllClass, getCoin, getSubject } from "~/services/workspaces.sevices";
+import LoadingOverlay from 'react-loading-overlay';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 
 
 const cx = classNames.bind(styles);
-
 
 function RegisterAsTutor(props) {
     const [subjects, setSubjects] = useState([])
     const [levels, setLevels] = useState([])
     const [coin, setCoins] = useState()
+    const [loading, setLoading] = useState(false)
     const [level, setLevel] = useState('STUDENT')
+
     useEffect(() => {
         fetchSubject(subjects).then(subject => setSubjects(subject));
         fetchLevel().then(res => setLevels(res))
@@ -44,7 +47,9 @@ function RegisterAsTutor(props) {
             );
         }
     };
+
     const fetchBecomeTutor = async () => {
+        setLoading(true)
         const body = {
             "level": level,
             "subjectIds": val
@@ -55,14 +60,19 @@ function RegisterAsTutor(props) {
         }
         else {
             const res = await becomeTutor(body)
+
             console.log(res);
-            if (res.status !== 200) {
-                alert('Đăng ký thất bại, vui lòng thử lại!!!')
+            setLoading(false)
+
+            if (res.data.status === 1) {
+                NotificationManager.success(res.data.message);
             }
-            else if (res.data.stutus === 1) {
-                alert('Đăng ký thành công!!!')
+            else {
+                NotificationManager.error(res.data.message);
             }
+
         }
+
     }
     const fetchCoin = async () => {
 
@@ -71,76 +81,79 @@ function RegisterAsTutor(props) {
     }
     useEffect(() => {
         fetchCoin()
-
     }, [])
     return (
         <>
-            <div className={cx('wrapper')}>
-                <Container>
-                    <Row className='d-flex flex-row'>
-                        <Col lg={6} className='pe-5'>
-                            <h3 className={cx('title', 'line-bottom')}>Thông tin Gia sư</h3>
-                            <Col lg={12} className={cx('item')}>
-                                <Form.Label
-                                    className={cx('description')}> Bạn đang là <FaStarOfLife className={cx('icon-label')} />
-                                </Form.Label>
-                                <Form.Select className={cx('list-area', 'fs-4')} value={level} onChange={e => setLevel(e.target.value)} placeholder='Trình độ'>
-                                    {levels.map((item, index) => {
-                                        return (
-                                            <option value={item.id} key={index}>{item.name}</option>
-                                        )
-                                    })}
+            <LoadingOverlay active={loading} spinner text="Đang xử lý...">
+                <div className={cx('wrapper')}>
+                    <Container>
+                        <Row className='d-flex flex-row'>
+                            <Col lg={6} className='pe-5'>
+                                <h3 className={cx('title', 'line-bottom')}>Thông tin Gia sư</h3>
+                                <Col lg={12} className={cx('item')}>
+                                    <Form.Label
+                                        className={cx('description')}> Bạn đang là <FaStarOfLife className={cx('icon-label')} />
+                                    </Form.Label>
+                                    <Form.Select className={cx('list-area', 'fs-4')} value={level} onChange={e => setLevel(e.target.value)} placeholder='Trình độ'>
+                                        {levels.map((item, index) => {
+                                            return (
+                                                <option value={item.id} key={index}>{item.name}</option>
+                                            )
+                                        })}
 
-                                </Form.Select>
+                                    </Form.Select>
+                                </Col>
+                                <Col lg={12} className={cx('item')}>
+                                    <Form.Label
+                                        className={cx('description')}>Môn dạy <FaStarOfLife className={cx('icon-label')} />
+                                    </Form.Label>
+                                    <Row className='d-flex'>
+                                        {subjects.map((item, index) => {
+                                            return (
+                                                <Col lg={3} className="form-check" key={index}>
+                                                    <input
+                                                        className="form-check-input"
+                                                        type="checkbox"
+                                                        id="flexCheckDefault"
+                                                        value={item.id}
+                                                        onChange={handleChange}
+                                                    />
+                                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                                        {item.name}
+                                                    </label>
+                                                </Col>
+
+                                            )
+                                        })}
+                                    </Row>
+                                </Col>
+                                <Col lg={12}>
+                                    <center>
+                                        <Button
+                                            style={{ marginTop: '20px' }}
+                                            className={cx('btn', 'btn-success px-4 py-3')}
+                                            size="lg" onClick={() => fetchBecomeTutor()}>
+                                            Đăng ký làm gia sư
+                                        </Button>
+                                    </center>
+                                </Col>
                             </Col>
-                            <Col lg={12} className={cx('item')}>
-                                <Form.Label
-                                    className={cx('description')}>Môn dạy <FaStarOfLife className={cx('icon-label')} />
-                                </Form.Label>
-                                <Row className='d-flex'>
-                                    {subjects.map((item, index) => {
-                                        return (
-                                            <Col lg={3} className="form-check" key={index}>
-                                                <input
-                                                    className="form-check-input"
-                                                    type="checkbox"
-                                                    id="flexCheckDefault"
-                                                    value={item.id}
-                                                    onChange={handleChange}
-                                                />
-                                                <label className="form-check-label" htmlFor="flexCheckDefault">
-                                                    {item.name}
-                                                </label>
-                                            </Col>
-
-                                        )
-                                    })}
-                                </Row>
+                            <Col lg={6} className="card text-white py-5">
+                                <div className={cx("overlay", "card-img-overlay d-flex flex-column align-items-center justify-content-center")}>
+                                    <h5 className={cx("text-title", "card-title fw-bold fs-1")}>ĐĂNG KÝ LÀM GIA SƯ</h5>
+                                    <p className="card-text text-center fs-2">
+                                        Để trở thành gia sư, bạn cần cung cấp thêm thông tin vào bên dưới,<br />bạn cần chi trả <span className={cx('text-coin', ' fs-1 fw-bold')}>48 coin <FaCoins /></span> để trở thành gia sư.
+                                    </p>
+                                </div>
                             </Col>
-                            <Col lg={12}>
-                                <center>
-                                    <Button
-                                        style={{ marginTop: '20px' }}
-                                        className={cx('btn', 'btn-success')}
-                                        size="lg" onClick={() => fetchBecomeTutor()}>
-                                        Đăng ký làm gia sư
-                                    </Button>
-                                </center>
-                            </Col>
-                        </Col>
-                        <Col lg={6} className="card text-white py-5">
-                            <div className={cx("overlay", "card-img-overlay d-flex flex-column align-items-center justify-content-center")}>
-                                <h5 className={cx("text-title", "card-title fw-bold fs-1")}>ĐĂNG KÝ LÀM GIA SƯ</h5>
-                                <p className="card-text text-center fs-2">
-                                    Để trở thành gia sư, bạn cần cung cấp thêm thông tin vào bên dưới,<br />bạn cần chi trả <span className={cx('text-coin', ' fs-1 fw-bold')}>48 coin <FaCoins /></span> để trở thành gia sư.
-                                </p>
-                            </div>
-                        </Col>
 
-                    </Row>
-                </Container>
+                        </Row>
+                    </Container>
 
-            </div>
+                </div>
+                <NotificationContainer />
+            </LoadingOverlay>
+
         </>
 
     );
