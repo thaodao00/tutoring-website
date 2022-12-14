44a570445aff833c8b59418f770a26fe15854486
Đào Thị Thu Thảo <91473426+thaodao00@users.noreply.github.com>
2022-12-14 11:45:02 +0700

    ++ apply class

--- src/pages/Class/ClassItem/ClassItem.js
+++ src/pages/Class/ClassItem/ClassItem.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Row } from "react-bootstrap";
-import { FaAngleDoubleRight, FaBook, FaMapMarkerAlt } from "react-icons/fa";
+import { FaAngleDoubleRight, FaBook, FaCoins, FaMapMarkerAlt } from "react-icons/fa";
 import classNames from 'classnames/bind';
 
 import styles from './ClassItem.module.scss';
@@ -8,11 +8,19 @@ import { AiOutlineClockCircle, AiOutlineSchedule } from "react-icons/ai";
 import { BsGenderAmbiguous } from "react-icons/bs";
 import { FiUser } from "react-icons/fi";
 import dayjs from "moment";
-
-
+import { NotificationContainer, NotificationManager } from 'react-notifications';
+import LoadingOverlay from 'react-loading-overlay';
+import { async } from '@firebase/util';
+import { applyClass } from '~/services/workspaces.sevices'
+import { useSelector } from 'react-redux';
 const cx = classNames.bind(styles);
 
 function ClassItem(props) {
+    const { user } = useSelector((state) => state.auth)
+
+    const [loading, setLoading] = useState(false)
+
+
     const convertToDate = (date) => {
         const newDate = new Date(date)
         dayjs.locale('vi')
@@ -24,105 +32,141 @@ function ClassItem(props) {
 
 
     const { data } = props;
-    console.log(data)
+
     const formatCurrency = (currency) => {
         currency = currency.toLocaleString('vi', { style: 'currency', currency: 'VND' });
         return currency
     }
+    function SwitchCase(props) {
 
+        switch (props.value) {
+
+            case 'ALL':
+                return <p>Nam hoăc Nữ</p>
+            case 'MALE':
+                return <p>Nam</p>
+
+            case 'FEMALE':
+                return <p>Nữ</p>
+            default:
+                return <p>""</p>
+
+        }
+    }
+    const handleClick = async () => {
+        setLoading(true)
+        const body = {
+            userApplyId: user.id,
+            classId: data.id,
+            createBy: data.createdBy.id
+        }
+
+        await applyClass(body).then((res) => {
+            if (res.data.status === 1) {
+                NotificationManager.success(res.status.message)
+            }
+            else {
+                NotificationManager.error(res.data.message)
+            }
+        })
+            .catch(e => NotificationManager.error(e.response.data.message))
+        setLoading(false)
+
+    }
     return (
-        <div className={cx('course-block', 'clear-fix')}>
-            <Col lg={12} md={12}>
-                <div className={cx('line-bottom')}>
-                    <h3 className={cx('title')}>{data.title}</h3>
-                    <div className={cx('code-block')}>
-                        <b className={cx('code')}>Mã lớp: {data.id}</b>
-                        <FaAngleDoubleRight
-                            className={cx('icon-arrow', 'animate__animated animate__fadeInLeft animate__infinite infinite')}
-                        />
+        <LoadingOverlay active={loading} spinner text="Đang xử lý...">
+            <div className={cx('course-block', 'clear-fix')}>
+                <Col lg={12} md={12}>
+                    <div className={cx('line-bottom')}>
+                        <h3 className={cx('title')}>{data.title}</h3>
+                        <div className={cx('code-block')}>
+                            <b className={cx('code')}>Mã lớp: {data.id}</b>
+                            <FaAngleDoubleRight
+                                className={cx('icon-arrow', 'animate__animated animate__fadeInLeft animate__infinite infinite')}
+                            />
 
+                        </div>
                     </div>
-                </div>
-                <div className={cx('fee-block')}>
+                    <div className={cx('fee-block')}>
+                        <Row>
+                            <Col lg={6} md={12}>
+                                <h4 className={cx('fee')}><span>Học phí :</span> {formatCurrency(data.tuition)}/Tháng</h4>
+                            </Col>
+                            <Col lg={6} md={12}>
+                                { }
+                                <button className={cx('btn-take')} onClick={handleClick} disabled={data.status === "CREATE" ? false : true}>
+                                    <span> Phí: <b>20% ({formatCurrency(data.tuition * 0.2 / 1)})</b></span>
+                                    <div></div>
+                                    <b>Quy đổi {Math.round(data.tuition * 0.2 / 1) / 1000} coin <FaCoins className='text-warning' /></b>
+                                    <div></div>
+                                    {data.status === "CREATE" ? (<span>Nhận lớp ngay</span>) : (<span>Lớp đã có người nhận</span>)}
+
+                                </button>
+                            </Col>
+                        </Row>
+                    </div>
+                    <div className={cx('description')}>
+                        <p>{data.description}</p>
+                    </div>
                     <Row>
-                        <Col lg={6} md={12}>
-                            <h4 className={cx('fee')}><span>Học phí :</span> {formatCurrency(data.tuition)}/Tháng</h4>
+                        {/*<Col lg={4} className={cx('request-block')}>*/}
+                        {/*    <AiOutlineClockCircle className={cx('icon-request')}/>*/}
+                        {/*    <span><b>Tạo lúc:</b> 12:06 29.10.2022</span>*/}
+                        {/*</Col>*/}
+                        <Col lg={4} className={cx('request-block')}>
+                            <BsGenderAmbiguous className={cx('icon-request')} />
+                            <span><b>Yêu cầu gia sư:</b><SwitchCase value={data.classRequirement.genderTutor} /></span>
                         </Col>
-                        <Col lg={6} md={12}>
-                            <a href='' className={cx('btn-take')}>
-                                <span> Phí: <b>20% ({formatCurrency(data.tuition + data.tuition * 0.25)})</b></span>
-                                <span>Nhận lớp ngay</span>
-                            </a>
+                        <Col lg={4} className={cx('request-block')}>
+                            <AiOutlineSchedule className={cx('icon-request')} />
+                            <span>Tuần học <b>{data.requireRelationshipTimeWeeks.length}</b> buổi
+                                ({convertToMinute(data.classRequirement.timeLesson)} phút/buổi)</span>
                         </Col>
                     </Row>
-                </div>
-                <div className={cx('description')}>
-                    <p>{data.description}</p>
-                </div>
-                <Row>
-                    {/*<Col lg={4} className={cx('request-block')}>*/}
-                    {/*    <AiOutlineClockCircle className={cx('icon-request')}/>*/}
-                    {/*    <span><b>Tạo lúc:</b> 12:06 29.10.2022</span>*/}
-                    {/*</Col>*/}
-                    <Col lg={4} className={cx('request-block')}>
-                        <BsGenderAmbiguous className={cx('icon-request')} />
-                        <span><b>Yêu cầu gia sư:</b> {data.classRequirement.genderTutor}(degree)</span>
-                    </Col>
-                    <Col lg={4} className={cx('request-block')}>
-                        <AiOutlineSchedule className={cx('icon-request')} />
-                        <span>Tuần học <b>{data.requireRelationshipTimeWeeks.length}</b> buổi
-                            ({convertToMinute(data.classRequirement.timeLesson)} phút/buổi)</span>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg={12} className={cx('request-block')}>
-                        <FiUser className={cx('icon-request')} />
-                        <span
-                            style={{ fontSize: '1.4rem' }}><b>{data.classRequirement.amountStudent}</b> học viên (nam)</span>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg={12} sm={12}>
-                        <FaMapMarkerAlt className={cx('icon-request')} />
-                        <span style={{ fontSize: '1.4rem' }}>
-                            <b>Địa điểm dạy:</b>
-                            {data.classRequirement.address.fullAddress}
-                            <a href="" className={cx('map')}>(Xem bản đồ )</a>
-                        </span>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg={12} sm={12}>
-                        <AiOutlineClockCircle className={cx('icon-request')} />
-                        <span style={{ fontSize: '1.4rem' }}>
-                            <b>Thời gian rãnh:</b>
-                            {data.requireRelationshipTimeWeeks.map((item) => item.date)}
+                    <Row>
+                        <Col lg={12} className={cx('request-block')}>
+                            <FiUser className={cx('icon-request')} />
+                            <span
+                                style={{ fontSize: '1.4rem' }}><b>{data.classRequirement.amountStudent}</b> học viên</span>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={12} sm={12}>
+                            <FaMapMarkerAlt className={cx('icon-request')} />
+                            <span style={{ fontSize: '1.4rem' }}>
+                                <b>Địa điểm dạy:</b>
+                                {data.classRequirement.address.fullAddress}
+                            </span>
+                        </Col>
+                    </Row>
 
-                        </span>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg={12} sm={12}>
-                        <FaBook className={cx('icon-request')} />
-                        <span style={{ fontSize: '1.4rem' }}>
-                            <b> Môn học:</b>
-                            <strong
-                                style={{
-                                    // borderLeft: '1px solid #ccc',
-                                    color: '#333',
-                                    fontWeight: 300,
-                                    marginLeft: '8px',
-                                    paddingLeft: '8px'
-                                }}>
-                                {data.subject.name}
+                    <Row>
+                        <Col lg={12} sm={12}>
+                            <FaBook className={cx('icon-request')} />
+                            <span style={{ fontSize: '1.4rem' }}>
+                                <b> Môn học:</b>
+                                <strong
+                                    style={{
+                                        // borderLeft: '1px solid #ccc',
+                                        color: '#333',
+                                        fontWeight: 300,
+                                        marginLeft: '8px',
+                                        paddingLeft: '8px'
+                                    }}>
+                                    {data.subject.name}
 
-                            </strong>
-                        </span>
-                    </Col>
-                </Row>
-            </Col>
+                                </strong>
+                            </span>
+                        </Col>
+                    </Row>
+                </Col>
 
-        </div>
+            </div>
+            <NotificationContainer />
+
+        </LoadingOverlay>
+
+
     );
 }
 
